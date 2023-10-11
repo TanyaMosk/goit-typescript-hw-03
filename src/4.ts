19de295dff0b085670aca9f67fe7b9a2bc42def4
TanyaMosk <tanya856227@gmail.com>
2023-10-11 11:22:18 +0200

    Update 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,55 @@
+class Key {
+    private signature: number;
+    
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature(){
+        return this.signature;
+    }
+};
+
+class Person {
+    private key: Key;
+
+    constructor(key:Key) {
+        this.key = key;
+     }
+    
+    getKey() {
+        return this.key;
+    }    
+};
+
+abstract class House {
+    door = false;
+    key: Key;
+    tenants: Person[] = [];  
+
+    constructor(key: Key) {
+        this.key = key;
+    }   
+
+    comeIn(person: Person) {
+        if (this.door === true) {            
+            this.tenants.push(person);
+        }        
+    };
+
+    abstract openDoor(key: Key): boolean;
+};
+
+class MyHouse extends House{
+
+   openDoor(key: Key): boolean {
+        if (this.key === key) {
+            this.door = true;
+            return true;
+       }             
+       return false;
+    }
+};    
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +59,5 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
+export { };
 
-export {};
